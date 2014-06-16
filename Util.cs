60a4d4b6f890938405012428bf8186911f19ff0b
hakomo <hakomoa@gmail.com>
2014-06-16 23:39:14 +0900

    add

--- Util.cs
+++ Util.cs
@@ -7,13 +7,34 @@ namespace UtilN {
 
     public class Util {
 
+        private static Random r = new Random();
+
         public static void Add(Control c, Control d, int ix) {
             c.Controls.Add(d);
             c.Controls.SetChildIndex(d, ix);
         }
 
+        public static int BinarySearch(int l, int r, Predicate<int> p) {
+            while(r - l > 1) {
+                if(p((l + r) / 2)) {
+                    l = (l + r) / 2;
+                } else {
+                    r = (l + r) / 2;
+                }
+            }
+            return l;
+        }
+
         public static MeasureTimer Mt(string s = "") {
             return new MeasureTimer(s);
+        }
+
+        public static int Rand(int n) {
+            return r.Next(n);
+        }
+
+        public static int Rand(int n, int m) {
+            return r.Next(m - n) + n;
         }
 
         public static void Run(Action a) {
@@ -49,6 +70,62 @@ namespace UtilN {
             public void Dispose() {
                 sw.Stop();
                 Console.WriteLine("{0, 5} ms : {1}", sw.ElapsedMilliseconds, s);
+            }
+        }
+    }
+
+    public class Bit {
+
+        private readonly int[] a;
+
+        public Bit(int n) {
+            a = new int[n];
+        }
+
+        public void Update(int i, int p) {
+            for(; i < a.Length; i |= i + 1)
+                a[i] += p;
+        }
+
+        public int this[int i] {
+            get {
+                int sm = 0;
+                for(; i >= 0; i = (i & (i + 1)) - 1)
+                    sm += a[i];
+                return sm;
+            }
+        }
+    }
+
+    public class UnionFind {
+
+        private readonly int[] a, b;
+
+        public UnionFind(int n) {
+            int i;
+            a = new int[n];
+            b = new int[n];
+            for(i = 0; i < n; ++i)
+                a[i] = i;
+        }
+
+        private int Find(int i) {
+            a[i] = i == a[i] ? i : Find(a[i]);
+            return a[i];
+        }
+
+        public bool Same(int i, int j) {
+            return Find(i) == Find(j);
+        }
+
+        public void Union(int i, int j) {
+            i = Find(i);
+            j = Find(j);
+            if(b[i] < b[j]) {
+                a[i] = j;
+            } else {
+                a[j] = i;
+                b[i] += b[i] == b[j] ? 1 : 0;
             }
         }
     }
